9d6554cd66635e96bf1cbb495d2d20a752b40cdf
sakir tufan <sakirtufan49@gmail.com>
2021-03-17 12:12:12 +0100

    Implement Add To Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router,Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <Router>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               shopper
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Wagen</a>
-            <a href="/signin">Einloggen</a>
+            <Link to="/cart">
+              Einkaufswagen
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Einloggen</Link>
           </div>
         </header>
         <main>
           <Route path="/" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen}/>          
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen}></Route>
         </main>
         <footer className="row center">Alle Rechte vorbehalten</footer>
       </div>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {detailsProduct } from "../redux/actions/productAction";
@@ -9,12 +9,17 @@ import Rating from "../components/Rating";
 export default function ProductScreen(props) {
   const dispatch = useDispatch();
   const productId = props.match.params.id;
+  const [qty, setQty] = useState(1);
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
   useEffect(() => {
     dispatch(detailsProduct(productId));
   }, [dispatch, productId]);
+
+   const addToCartHandler = () => {
+     props.history.push(`/cart/${productId}?qty=${qty}`);
+   };
 
   return (
     <div>
@@ -72,11 +77,31 @@ export default function ProductScreen(props) {
                       </div>
                     </div>
                   </li>
-                  <li>
-                    <button className="primary block">
-                      In den Einkaufswagen
-                    </button>
-                  </li>
+                  {product.countInStock > 0 && (
+                    <>
+                      <li>
+                        <div className="row">
+                          <div>Menge</div>
+                          <div>
+                            <select value={qty} onChange={e=>setQty(e.target.value)}>
+                              {
+                                [...Array(product.countInStock).keys()].map(
+                                  (x) => (
+                                    <option key={x+1} value={x+1}>{x+1}</option>
+                                  )
+                                )
+                              }
+                            </select>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <button onClick={addToCartHandler} className="primary block">
+                          In den Einkaufswagen
+                        </button>
+                      </li>
+                    </>
+                  )}
                 </ul>
               </div>
             </div>
